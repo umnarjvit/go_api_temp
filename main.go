13ba0e0f462faee46e76ceb3e04acbfefd4dcd53
Lukplarnoi <69046206+umnarjvit@users.noreply.github.com>
2022-06-10 22:47:51 +0700

    Update main.go

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-//	router.LoadHTMLGlob("templates/**/**/*.tmpl.html")
+//	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/**/**/*.tmpl.html")
 	router.Static("/static", "static")
 	
 	router.GET("/login", ShowLogin) // login page
@@ -32,7 +32,7 @@ func main() {
 			c.HTML(http.StatusOK, "index.tmpl.html", nil)
 		})
 	router.GET("/", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "login.tmpl.html", nil)
+			c.HTML(http.StatusOK, "teml/teml/login.tmpl.html", nil)
 		})
 	
 	/*
