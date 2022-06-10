75f08bcf9d1299a2e0469b668a20fdda30bf74e2
Lukplarnoi <69046206+umnarjvit@users.noreply.github.com>
2022-06-10 21:40:44 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/**/**/*.tmpl.html")
+	router.Static("/static", "static")
+	router.GET("/login", ShowLogin) // login page
+	/*
+		router.GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		})
+	*/
+	router.NoRoute(func(c *gin.Context) {
+		// set location
+		location := url.URL{Path: "/login"}
+		c.Redirect(http.StatusFound, location.RequestURI())
+
+	})
+	router.Run(":" + port)
+}
+func ShowLogin(c *gin.Context) {
+	cc := gin.H{"title": "หน้าแรก"}
+	c.HTML(http.StatusOK, "teml/teml/login.tmpl.html", cc)
+
+	c.Abort()
+
+}
