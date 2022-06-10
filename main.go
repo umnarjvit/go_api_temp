18bfe290cd9bdcd72447e15ff18720114074dbba
Lukplarnoi <69046206+umnarjvit@users.noreply.github.com>
2022-06-10 21:55:30 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -18,23 +19,24 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	//router.LoadHTMLGlob("templates/**/**/*.tmpl.html")
+	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/**/**/*.tmpl.html")
 	router.Static("/static", "static")
+	
 	router.GET("/login", ShowLogin) // login page
 	/*
 		router.GET("/", func(c *gin.Context) {
 			c.HTML(http.StatusOK, "index.tmpl.html", nil)
 		})
 	*/
-	/*
+	
 	router.NoRoute(func(c *gin.Context) {
 		// set location
 		location := url.URL{Path: "/login"}
 		c.Redirect(http.StatusFound, location.RequestURI())
 
 	})
-	*/
+	
 	router.Run(":" + port)
 }
 func ShowLogin(c *gin.Context) {
